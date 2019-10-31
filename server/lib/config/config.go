c83ee9c0e050ce43448a058d3aeaf5470259da7e
nitoling <nitoling@gmail.com>
2019-10-31 23:27:43 +0900

    #6 ConfigLoaderの構造体が変な感じだったので修正

--- server/lib/config/config.go
+++ server/lib/config/config.go
@@ -10,8 +10,8 @@ type Config struct {
 	// MySQL
 	MySQLHost     string `environment_key:"TORCH_MYSQL_HOST" type:"string"`
 	MySQLPort     int    `environment_key:"TORCH_MYSQL_PORT" type:"int"`
-	MySQLUser     string `environment_key:"TORCH_API_USER" type:"string"`
-	MySQLPassword string `environment_key:"TORCH_API_PASS" type:"string"`
+	MySQLUser     string `environment_key:"TORCH_MYSQL_USER" type:"string"`
+	MySQLPassword string `environment_key:"TORCH_MYSQL_PASS" type:"string"`
 }
 
 func (config *Config) GetAPIServerLocation() string {
