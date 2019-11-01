f07dcac0ed29417d4a05f7f2009ab905ebc00fa4
nitoling <nitoling@gmail.com>
2019-11-02 03:45:12 +0900

    #20 画像の保存用モデルの実装を書いた

---
+++ server/model/spot_photo.go
@@ -0,0 +1,57 @@
+package model
+
+import (
+	"fmt"
+	"github.com/google/uuid"
+	"io"
+	"mime/multipart"
+	"os"
+	"path"
+)
+
+type SpotPhotoModelImpl struct {
+}
+
+func (model *SpotPhotoModelImpl) Add(image *multipart.FileHeader) (fileName string, err error) {
+	// 画像のの読み込み
+	srcFileName := image.Filename
+	srcFileExt := path.Ext(srcFileName)
+	srcFile, err := image.Open()
+	if err != nil {
+		return "", err
+	}
+	defer srcFile.Close()
+
+	// 書き込み先ディレクトリがあるか確認し、なければ作る
+	dstFileName, dstDir, dstFilePath := model.generateFilePathSet(srcFileExt)
+	if _, err := os.Stat(dstDir); os.IsNotExist(err) {
+		err = os.Mkdir(dstDir, 0777)
+		if err != nil {
+			return "", nil
+		}
+	}
+
+	// 書き込む画像をとりあえず作る
+	dstFile, err := os.Create(dstFilePath)
+	if err != nil {
+		return "", err
+	}
+	defer dstFile.Close()
+
+	// 書き込む
+	_, err = io.Copy(dstFile, srcFile)
+	if err != nil {
+		return "", err
+	}
+
+	return dstFileName, nil
+}
+
+func (model *SpotPhotoModelImpl) generateFilePathSet(ext string) (fileName, pathDir, filePath string) {
+	uuidStr := uuid.New().String()
+	fileName = fmt.Sprintf("%s.%s", uuidStr, ext)
+	pathDir = "./assets/spots/"
+	filePath = path.Join("./assets/spots/", fileName)
+
+	return fileName, pathDir, filePath
+}
