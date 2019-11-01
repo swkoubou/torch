e784e0525ab3a90c8ba913db70808b7d70702e91
nitoling <nitoling@gmail.com>
2019-11-02 06:24:35 +0900

    #25 ルーティングにSpotInfoPbViewを追加

--- server/route.go
+++ server/route.go
@@ -3,18 +3,28 @@ package main
 import (
 	"github.com/jinzhu/gorm"
 	"github.com/labstack/echo"
+	"github.com/swkoubou/torch/server/lib/pbrouter"
+	"github.com/swkoubou/torch/server/model"
+	"github.com/swkoubou/torch/server/view/pb"
 	"net/http"
 )
 
 func Route(e *echo.Echo, db *gorm.DB) error {
-	// ControllerやModelの準備
+	// ViewやModelの準備
+	spotPhotoModel := model.NewSpotPhotoModel()
+	spotModel := model.NewSpotModel(db, spotPhotoModel)
 
+	spotInfoPbView := pb.NewSpotInfoPbView(spotModel)
 
 	// ルーティング
 	// これはテスト用にHello, Worldしてる
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
+
+	// Protocol Buffersルーターによるルーティング
+	pbRouter := pbrouter.NewProtocolBufferRouterByRouter(e)
+	pbRouter.POST("/spot/info", spotInfoPbView.GetPublishInterface(), spotInfoPbView.GetPOSTHandler)
 
 	return nil
 }
