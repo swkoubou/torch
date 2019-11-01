106c214a1c23c2e28cea123b4ac47546bc2e2ca6
nitoling <nitoling@gmail.com>
2019-11-02 08:57:12 +0900

    #39 スポットのいいねを行う関数を実装

--- server/model/like_spot.go
+++ server/model/like_spot.go
@@ -1,6 +1,8 @@
 package model
 
 import (
+	"errors"
+	"fmt"
 	"github.com/jinzhu/gorm"
 	"github.com/swkoubou/torch/server/model/types"
 )
@@ -9,8 +11,24 @@ type LikeSpotModelImpl struct {
 	db *gorm.DB
 }
 
-func (model *LikeSpotModelImpl) Like(spotID uint) (count uint, err error) {
-	panic("implement me")
+func (model *LikeSpotModelImpl) Like(spotID uint) (err error) {
+	adding := &types.LikeSpot{
+		SpotInfoID: spotID,
+	}
+
+	result := model.db.Create(&adding)
+	err = result.Error
+	if err != nil {
+		errMsg := "LikeSpotModel.Like(): " + err.Error()
+		return errors.New(errMsg)
+	}
+	affected := result.RowsAffected
+	if affected <= 0 {
+		errMsg := fmt.Sprintf("LikeSpotModel.Like(): LikeSpot(SpotID: %s) can't be added.", adding.SpotInfoID)
+		return errors.New(errMsg)
+	}
+
+	return nil
 }
 
 func (model *LikeSpotModelImpl) CountLikes(spotID uint) (count uint, err error) {
