ad889fdeea1f00a1f5464f58392a498c7a1a54ee
NISHI3 <18317954+NISHI3@users.noreply.github.com>
2019-11-02 08:24:03 +0900

    #59 WIP

---
+++ web/pages/privacy.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "privacy"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ web/pages/term.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-card class="overflow-hidden">
+    <v-app-bar dark color="primary">
+      <v-btn icon dark @click="backButton">
+        <v-icon>mdi-arrow-left</v-icon>
+      </v-btn>
+      <v-toolbar-title>規約</v-toolbar-title>
+    </v-app-bar>
+　</v-card>
+</template>
+
+<script>
+    export default {
+        name: "term"
+    }
+</script>
+
+<style scoped>
+
+</style>
