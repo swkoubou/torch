c4093729fd9782a7652d6718d6bce4aca9638b35
moezakura <mox@mox.si>
2019-11-01 19:13:33 +0900

    #12 変数名が間違っていたため修正

--- web/components/pinDetail.vue
+++ web/components/pinDetail.vue
@@ -5,14 +5,14 @@
         <v-btn icon dark @click="dialogClose">
           <v-icon>mdi-close</v-icon>
         </v-btn>
-        <v-toolbar-title>{{ spotName }}の情報</v-toolbar-title>
+        <v-toolbar-title>{{ pinName }}の情報</v-toolbar-title>
         <v-spacer></v-spacer>
         <v-toolbar-items>
           <v-btn dark text @click="dialogClose">閉じる</v-btn>
         </v-toolbar-items>
       </v-toolbar>
 
-      <v-img height="300" :src="spotImageUrl"></v-img>
+      <v-img height="300" :src="pinImageUrl"></v-img>
 
       <v-flex class="d-flex px-2">
         <!-- 各種SNSシェア -->
@@ -34,12 +34,12 @@
           <v-btn fab>
             <v-icon color="primary">mdi-heart</v-icon>
           </v-btn>
-          <p class="caption text-center" style="color: #888888">{{ spotFavCount }}</p>
+          <p class="caption text-center" style="color: #888888">{{ pinFavCount }}</p>
         </div>
       </v-flex>
 
       <div class="my-3">
-        <pre class="text-wrap">{{ spotDetailText }}</pre>
+        <pre class="text-wrap">{{ pinDetailText }}</pre>
       </div>
 
       <!-- ユーザー投稿欄 -->
@@ -66,19 +66,19 @@
             };
         },
         computed: {
-            spotFavCount(): number {
+            pinFavCount(): number {
                 return 1204;
             },
-            spotName(): string {
-                //TODO: スポットの名前を返す
+            pinName(): string {
+                //TODO: ピンの名前を返す
                 return 'test';
             },
-            spotImageUrl(): string {
-                //TODO: スポットの画像を返す
+            pinImageUrl(): string {
+                //TODO: ピンの画像を返す
                 return 'https://dummyimage.com/600x400/000/fff';
             },
-            spotDetailText(): string {
-                //TODO: スポットのテキストを返す
+            pinDetailText(): string {
+                //TODO: ピンのテキストを返す
                 return '　吾輩は猫である。名前はまだ無い。\n' +
                     '　どこで生れたかとんと見当がつかぬ。何でも薄暗いじめじめした所でニャーニャー泣いていた事だけは記憶している。吾輩はここで始めて人間というものを見た。しかもあとで聞くとそれは書生という人間中で一番獰悪な種族であったそうだ。この書生というのは時々我々を捕えて煮て食うという話である。しかしその当時は何という考もなかったから別段恐しいとも思わなかった。ただ彼の掌に載せられてスーと持ち上げられた時何だかフワフワした感じがあったばかりである。掌の上で少し落ちついて書生の顔を見たのがいわゆる人間というものの見始であろう。この時妙なものだと思った感じが今でも残っている。第一毛をもって装飾されべきはずの顔がつるつるしてまるで薬缶だ。その後猫にもだいぶ逢ったがこんな片輪には一度も出会わした事がない。のみならず顔の真中があまりに突起している。そうしてその穴の中から時々ぷうぷうと煙を吹く。どうも咽せぽくて実に弱った。これが人間の飲む煙草というものである事はようやくこの頃知った。';
             }
