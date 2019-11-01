331d996b9d73cbd88814f71a62e174909adfe118
NISHI3 <18317954+NISHI3@users.noreply.github.com>
2019-11-01 23:05:16 +0900

    #29 area情報を追加

--- web/pages/index.vue
+++ web/pages/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <p class="area-text body-1 text-center">{{ areaName }}</p>
 
     <!-- いいねボタン -->
     <v-btn fixed bottom right fab large outlined color="primary" v-if="!isAdmin">
@@ -34,6 +35,19 @@
             if ('admin' in query) {
                 this.isAdmin = query['admin'] === 'hoo0Jaek8jooTeeti0eiciedeithougee4aexooGhaiNgieDa9gio6jaipeevach';
             }
+        },
+        computed: {
+            areaName(): string {
+                return 'area1';
+            }
         }
     });
 </script>
+
+<style lang="scss" scoped>
+  .area-text {
+    width: 10rem;
+    background-color: rgba(229,57,53,0.5);
+    border-radius: 1em;
+  }
+</style>
