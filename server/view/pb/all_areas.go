ea732ef591d9ed0d89d4f2760a033857b6dc71ac
nitoling <nitoling@gmail.com>
2019-11-02 10:09:52 +0900

    #39 すべてのエリアを取得するビューを実装

---
+++ server/view/pb/all_areas.go
@@ -0,0 +1,28 @@
+package pb
+
+import (
+	"github.com/golang/protobuf/proto"
+	"github.com/labstack/echo"
+	"github.com/swkoubou/torch/server/model"
+	"github.com/swkoubou/torch/server/view"
+)
+
+type AllAreasPbViewImpl struct {
+	allAreasModel model.AllAreasModel
+}
+
+func NewAllAreasPbView(allAreasModel model.AllAreasModel) view.AllAreasPbView {
+	return &AllAreasPbViewImpl{allAreasModel: allAreasModel}
+}
+
+func (view *AllAreasPbViewImpl) GetGETHandler(ctx echo.Context) (message proto.Message, err error) {
+	// モデルに投げる
+	founds, err := view.allAreasModel.Get()
+	if err != nil {
+		return nil, err
+	}
+
+	// レスポンスをつくる
+	response := ToAllAreaInfoResponse(founds)
+	return response, nil
+}
