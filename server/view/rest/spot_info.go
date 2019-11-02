0f1ee3dfd433cf3b37d4b1a1fcbd3fd46c168b52
nitoling <nitoling@gmail.com>
2019-11-02 12:46:24 +0900

    #26 スポットを追加するビューの実装を作成

--- server/view/rest/spot_info.go
+++ server/view/rest/spot_info.go
@@ -1,9 +1,13 @@
 package rest
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/labstack/echo"
 	"github.com/swkoubou/torch/server/model"
+	"github.com/swkoubou/torch/server/model/types"
 	"github.com/swkoubou/torch/server/view"
+	"github.com/swkoubou/torch/server/view/rest/messages"
 )
 
 type SpotInfoRESTViewImpl struct {
@@ -17,5 +21,49 @@ func NewSpotInfoRESTView(spotModel model.SpotModel) view.SpotInfoRESTView {
 }
 
 func (view *SpotInfoRESTViewImpl) GetPUTHandler(ctx echo.Context) (err error) {
-	panic("implement me")
+	// リクエストを読む(画像)
+	fileInfo, err := ctx.FormFile("image")
+	if err != nil {
+		_ = ctx.String(401, "failure")
+		fmt.Printf("SpotInfoRESTView.PUT(): %v\n", err)
+		return
+	}
+
+	// リクエストを読む(JSON)
+	optionJson := ctx.FormValue("option")
+	if err != nil {
+		_ = ctx.String(401, "failure")
+		fmt.Printf("SpotInfoRESTView.PUT(): %v\n", err)
+		return
+	}
+
+	/// JSONのUnmarshal
+	optionStruct := &messages.NewSpotInfoRequest{}
+	err = json.Unmarshal([]byte(optionJson), optionStruct)
+	if err != nil {
+		_ = ctx.String(401, "failure")
+		fmt.Printf("SpotInfoRESTView.PUT(): %v\n", err)
+		return
+	}
+
+	// 内部の型に書き換え
+	newSpotInfo := &types.NewSpotInfo{
+		Name:        optionStruct.Name,
+		Description: optionStruct.Description,
+		PhotoReader: fileInfo,
+		LocatedAt:   *optionStruct.GetLocation(),
+		AreaID:      optionStruct.AreaID,
+		StartingAt:  optionStruct.GetStartTime(),
+		EndingAt:    optionStruct.GetEndTime(),
+	}
+
+	err = view.spotModel.Add(newSpotInfo)
+	if err != nil {
+		_ = ctx.String(401, "failure")
+		fmt.Printf("SpotInfoRESTView.PUT(): %v\n", err)
+		return
+	}
+
+	_ = ctx.String(200, "success")
+	return nil
 }
