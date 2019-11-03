ca7c36a4d939182650c19df9a6684d13c15f0062
nitoling <nitoling@gmail.com>
2019-11-03 13:46:49 +0900

    #52 差を大きくした

--- server/model/hot_level.go
+++ server/model/hot_level.go
@@ -65,7 +65,7 @@ func (model *HotLevelModelImpl) alignMinMax(target, min, max float64) float64 {
 
 func (model *HotLevelModelImpl) calcHotLevel(likes, mean, variance, gravity float64) float64 {
 	k := 1.0
-	rawLevel := 200*(likes-mean)/(k*variance) + 10
+	rawLevel := 500*(likes-mean)/(k*variance) + 10
 	level := gravity * rawLevel
 
 	return model.alignMinMax(level, 0, 100)
