268b40aeeeff22d18b3ecb2167176f128a817d27
nitoling <nitoling@gmail.com>
2019-11-03 12:57:19 +0900

    #52 盛り上がり度合いを計算するようにした

--- server/model/hot_level.go
+++ server/model/hot_level.go
@@ -2,6 +2,7 @@ package model
 
 import (
 	"github.com/swkoubou/torch/server/model/types"
+	"gonum.org/v1/gonum/stat"
 )
 
 type HotLevelModelImpl struct{}
@@ -11,8 +12,15 @@ func NewHotLevelModel() HotLevelModel {
 }
 
 func (model *HotLevelModelImpl) CalcAreaHotLevel(targetAreas []types.AreaInfo, targetSpots []types.SpotInfo) (calculated []types.AreaInfo, err error) {
+	var likeArray []float64
 	for _, area := range targetAreas {
-		area.HotLevel = 1.0
+		likeArray = append(likeArray, float64(area.Likes))
+	}
+
+	mean, variance := stat.MeanVariance(likeArray, nil)
+
+	for _, area := range targetAreas {
+		model.calcHotLevel(float64(area.Likes), mean, variance, 1)
 		calculated = append(calculated, area)
 	}
 
@@ -20,10 +28,44 @@ func (model *HotLevelModelImpl) CalcAreaHotLevel(targetAreas []types.AreaInfo, t
 }
 
 func (model *HotLevelModelImpl) CalcSpotHotLevel(targetSpots []types.SpotInfo) (calculated []types.SpotInfo, err error) {
+	var data []float64
 	for _, spot := range targetSpots {
-		spot.HotLevel = 1.0
+		data = append(data, float64(spot.Likes))
+	}
+
+	mean, variance := stat.MeanVariance(data, nil)
+
+	for _, spot := range targetSpots {
+		gravity := model.calcSpotGravity(spot)
+
+		spot.HotLevel = model.calcHotLevel(float64(spot.Likes), mean, variance, gravity)
 		calculated = append(calculated, spot)
 	}
 
 	return calculated, nil
+}
+
+func (model *HotLevelModelImpl) calcSpotGravity(info types.SpotInfo) float64 {
+	duration := info.GetHourSpan()
+	rawGrav := -0.2173913*duration + 6.2173913
+
+	return model.alignMinMax(rawGrav, 0, 100)
+}
+
+func (model *HotLevelModelImpl) alignMinMax(target, min, max float64) float64 {
+	if min > target {
+		return min
+	}
+	if max < target {
+		return max
+	}
+
+	return target
+}
+
+func (model *HotLevelModelImpl) calcHotLevel(likes, mean, variance, gravity float64) float64 {
+	rawLevel := (likes - mean) / (2 * variance) * 100
+	level := gravity * rawLevel
+
+	return model.alignMinMax(level, 0, 100)
 }
